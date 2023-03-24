88cdfa0e2f238927d6e3f696839d6fd2e6b2d559
Shivam <goelshivam2012@gmail.com>
2023-03-25 00:52:50 +0530

    Final answer

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -34,10 +34,10 @@ public class SavingsAccount extends BankAccount{
         // 1. "Maximum Withdraw Limit Exceed" : If the amount exceeds maximum withdrawal limit
         // 2. "Insufficient Balance" : If the amount exceeds balance
 
-        if(amount > maxWithdrawalLimit)
-            throw new Exception("Maximum Withdraw Limit Exceed");
-        else
+        if(amount <= maxWithdrawalLimit)
           super.withdraw(amount);
+        else
+            throw new Exception("Maximum Withdraw Limit Exceed");
     }
 
     public double getSimpleInterest(int years){
